73cab36f16f14381ef0a95d3e4248f695938b386
HIBA ALQAED <hibaalqaed@Hibas-New-MacBook-Air.local>
2020-12-21 20:30:51 +0300

    almost done with signup

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const express = require("express");
+const userRoutes = require("./API/users/routes");
+const db = require("./db/models");
 
 const app = express();
 
-app.listen(8000, () => {
-  console.log("The application is running on localhost:8000");
-});
+app.use(userRoutes);
+
+const run = async () => {
+  try {
+    await db.sequelize.sync();
+    console.log("Connection to the database successful!");
+    await app.listen(8000, () => {
+      console.log("The application is running on localhost:8000");
+    });
+  } catch (error) {
+    console.error("Error connecting to the database: ", error);
+  }
+};
+
+run();
