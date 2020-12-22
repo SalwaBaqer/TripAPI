f43913246bc361a32f310d52d5af38afbd74caba
user <s.abaqer@hotmail.com>
2020-12-22 15:09:04 +0300

    triplist

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
 const express = require("express");
+const db = require("./db/models");
+const cors = require("cors");
+
+//routes
+const tripsRoutes = require("./API/trips/routes");
 
 const app = express();
 
-app.listen(8000, () => {
-  console.log("The application is running on localhost:8000");
-});
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use(tripsRoutes);
+
+const run = async () => {
+  try {
+    await db.sequelize.sync();
+    console.log("Connection to the database successful!");
+    await app.listen(8000, () => {
+      console.log("The application is running on localhost:8000");
+    });
+  } catch (error) {
+    console.error("Error connecting to the database: ", error);
+  }
+};
+
+run();
