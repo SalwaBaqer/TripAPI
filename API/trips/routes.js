caa5c4fc519615db7616c9507a6eba817348b406
user <s.abaqer@hotmail.com>
2020-12-23 20:25:01 +0300

    add and update

--- API/trips/routes.js
+++ API/trips/routes.js
@@ -1,13 +1,21 @@
 const express = require("express");
+//controllers
 const { tripsList, addtrip, updatetrip } = require("./controllers");
+
+//passport
+const passport = require("passport");
 
 const router = express.Router();
 
 router.get("/", tripsList);
 
 //Only user can add and update gotta get back once sign in works
-router.post("/", addtrip);
+router.post("/", passport.authenticate("jwt", { session: false }), addtrip);
 
-router.put("/:tripId", updatetrip);
+router.put(
+  "/:tripId",
+  passport.authenticate("jwt", { session: false }),
+  updatetrip
+);
 
 module.exports = router;
