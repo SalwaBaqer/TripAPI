1d473917c819f418bc5878aa1892cedf9ac679e5
SalwaBaqer <s.baqer@hotmail.com>
2020-12-24 19:22:06 +0300

    handling image

--- API/trips/controllers.js
+++ API/trips/controllers.js
@@ -29,6 +29,9 @@ exports.tripDelete = async (req, res) => {
 //add trip
 exports.addtrip = async (req, res) => {
   try {
+    if (req.file) {
+      req.body.image = `http://${req.get("host")}/media/${req.file.filename}`;
+    }
     req.body.UserId = req.user.id;
     const newTrip = await Trip.create(req.body);
     res.status(201).json(newTrip);
@@ -40,16 +43,29 @@ exports.addtrip = async (req, res) => {
 //update trip
 exports.updatetrip = async (req, res, next) => {
   try {
-    if (req.user.id === req.body.userId) {
+    console.log(
+      "req.user.id ",
+      req.user.id,
+      "req.body.userId",
+      req.body.userId
+    );
+
+    if (req.user.id === +req.body.userId) {
       const foundTrip = await Trip.findByPk(req.params.tripId);
+      //trip exist
       if (foundTrip) {
+        if (req.file) {
+          req.body.image = `http://${req.get("host")}/media/${
+            req.file.filename
+          }`;
+        }
         await foundTrip.update(req.body);
         res.status(201).json(foundTrip);
       } else {
         res.status(404).json({ message: "Trip not found" });
       }
     } else {
-      res.status(500).json({ message: "Unauthorized" });
+      res.status(401).json({ message: "Unauthorized" });
     }
   } catch (error) {
     next(error);
--- middleware/multer.js
+++ middleware/multer.js
@@ -2,7 +2,7 @@
 const multer = require("multer");
 
 const storage = multer.diskStorage({
-  destination: "../media",
+  destination: "./media",
   filename: (req, file, cb) => {
     cb(null, `${+new Date()} ${file.originalname}`);
   },
