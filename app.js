e62cbf8c4177e642f351bcdb81ef51759a4ea1c0
SalwaBaqer <s.baqer@hotmail.com>
2020-12-24 18:39:31 +0300

    mutler

--- app.js
+++ app.js
@@ -1,43 +1,43 @@
 /* Requires */
-const express = require('express')
+const express = require("express");
 
-const db = require('./db/models')
-const cors = require('cors')
-const passport = require('passport')
+const db = require("./db/models");
+const cors = require("cors");
+const passport = require("passport");
 // Passport Strategies
-const { localStrategy, jwtStrategy } = require('./middleware/passport')
+const { localStrategy, jwtStrategy } = require("./middleware/passport");
 
-const tripsRoutes = require('./API/trips/routes')
-const userRoutes = require('./API/users/routes')
+const tripsRoutes = require("./API/trips/routes");
+const userRoutes = require("./API/users/routes");
 /* Requires */
 
-const app = express()
+const app = express();
 
 // Middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 // Passport Setup
-app.use(passport.initialize())
-passport.use(localStrategy)
-passport.use(jwtStrategy)
+app.use(passport.initialize());
+passport.use(localStrategy);
+passport.use(jwtStrategy);
 
 // Routes
 
-app.use(tripsRoutes)
-app.use(userRoutes)
+app.use("/trips", tripsRoutes);
+app.use(userRoutes);
 
 const run = async () => {
   try {
-    await db.sequelize.sync({ alter: true })
-    console.log('Connection to the database successful!')
+    await db.sequelize.sync({ alter: true });
+    console.log("Connection to the database successful!");
 
     await app.listen(8000, () => {
-      console.log('The application is running on localhost:8000')
-    })
+      console.log("The application is running on localhost:8000");
+    });
   } catch (error) {
-    console.error('Error connecting to the database: ', error)
+    console.error("Error connecting to the database: ", error);
   }
-}
+};
 
-run()
+run();
