c96723879eec394e45fe0bb9bf84e7ed1f66716c
SalwaBaqer <s.baqer@hotmail.com>
2020-12-27 18:12:56 +0300

    handling errors

--- app.js
+++ app.js
@@ -1,53 +1,63 @@
-/* Requires */
-const express = require('express')
+const express = require("express");
 
-const db = require('./db/models')
-const cors = require('cors')
-const passport = require('passport')
+//db
+const db = require("./db/models");
+const cors = require("cors");
+
+//passport
+const passport = require("passport");
+
 // Passport Strategies
-const { localStrategy, jwtStrategy } = require('./middleware/passport')
+const { localStrategy, jwtStrategy } = require("./middleware/passport");
 
-const tripsRoutes = require('./API/trips/routes')
-const userRoutes = require('./API/users/routes')
-/* Requires */
+//routes
+const tripsRoutes = require("./API/trips/routes");
+const userRoutes = require("./API/users/routes");
 
-const app = express()
+//path
+const path = require("path");
+
+const app = express();
 
 // Middleware
-app.use(cors())
-app.use(express.json())
-// NOT FOUND PATH MIDDLEWARE
-// app.use((req, res, next) => {
-//   res.status(404).json({ messagae: "Path Not Found!" });
-// });
-// ERROR HANDLING MIDDLEWARE
-// app.use((err, req, res, next) => {
-//   res.status(err.status ?? 500);
-//   res.json({ message: err.message || "Internal Server Error" });
-// });
+app.use(cors());
+app.use(express.json());
+const mediaPath = path.join(__dirname, "media");
 
 // Passport Setup
-app.use(passport.initialize())
-passport.use(localStrategy)
-passport.use(jwtStrategy)
+app.use(passport.initialize());
+passport.use(localStrategy);
+passport.use(jwtStrategy);
 
 // Routes
 
-app.use('/trips', tripsRoutes)
-app.use(userRoutes)
+app.use("/trips", tripsRoutes);
+app.use("/media", express.static(mediaPath));
+app.use(userRoutes);
+
+//path not found
+app.use((req, res) => {
+  res.status(404).json({ message: "PATH NOT FOUND" });
+});
+
+//handling errors
+app.use((err, req, res, next) => {
+  res.status(err.status ?? 500);
+  res.json({ message: err.message ?? "Internal Server Error" });
+});
 
 const run = async () => {
   try {
-    await db.sequelize.sync({ alter: true })
+    await db.sequelize.sync({ alter: true });
     // await db.sequelize.sync({ force: true });
-    console.log('Connection to the database successful!')
+    console.log("Connection to the database successful!");
 
     await app.listen(8000, () => {
-      console.log('The application is running on localhost:8000')
-    })
+      console.log("The application is running on localhost:8000");
+    });
   } catch (error) {
-    console.error('Error connecting to the database: ', error)
+    console.error("Error connecting to the database: ", error);
   }
-}
+};
 
-run()
+run();
--- middleware/multer.js
+++ middleware/multer.js
@@ -4,7 +4,7 @@ const multer = require("multer");
 const storage = multer.diskStorage({
   destination: "./media",
   filename: (req, file, cb) => {
-    cb(null, `${+new Date()} ${file.originalname}`);
+    cb(null, `${+new Date()}${file.originalname}`);
   },
 });
 
